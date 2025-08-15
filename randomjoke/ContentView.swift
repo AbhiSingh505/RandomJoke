f94db5245c999eedb6975130c46b89c091fcfa38
Abhishek Singh <chouhanabhishek421@gmail.com>
2025-08-15 13:31:11 +0530

    Initial Commit

---
+++ randomjoke/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  randomjoke
+//
+//  Created by Abhishek Singh Chouhan on 15/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ randomjoke/randomjokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomjokeApp.swift
+//  randomjoke
+//
+//  Created by Abhishek Singh Chouhan on 15/08/25.
+//
+
+import SwiftUI
+
+@main
+struct randomjokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
